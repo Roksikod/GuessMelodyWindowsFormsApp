4603308325eac446fadb2f8463c27003cfb368c4
Roksana Zalesskaya <Ksroksana@bk.ru>
2023-11-17 23:22:56 +0300

    Added settings Form: to fill listMusic

--- GuessMelodyWindowsFormsApp/MainForm.Designer.cs
+++ GuessMelodyWindowsFormsApp/MainForm.Designer.cs
@@ -37,7 +37,7 @@
             // playButton
             // 
             this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.playButton.Location = new System.Drawing.Point(116, 55);
+            this.playButton.Location = new System.Drawing.Point(147, 39);
             this.playButton.Name = "playButton";
             this.playButton.Size = new System.Drawing.Size(168, 64);
             this.playButton.TabIndex = 0;
@@ -47,17 +47,18 @@
             // parametrsButton
             // 
             this.parametrsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.parametrsButton.Location = new System.Drawing.Point(116, 138);
+            this.parametrsButton.Location = new System.Drawing.Point(147, 141);
             this.parametrsButton.Name = "parametrsButton";
             this.parametrsButton.Size = new System.Drawing.Size(168, 64);
             this.parametrsButton.TabIndex = 1;
-            this.parametrsButton.Text = "Einstellungen";
+            this.parametrsButton.Text = "Regelung";
             this.parametrsButton.UseVisualStyleBackColor = true;
+            this.parametrsButton.Click += new System.EventHandler(this.parametrsButton_Click);
             // 
             // exitButton
             // 
             this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.exitButton.Location = new System.Drawing.Point(116, 230);
+            this.exitButton.Location = new System.Drawing.Point(147, 236);
             this.exitButton.Name = "exitButton";
             this.exitButton.Size = new System.Drawing.Size(168, 64);
             this.exitButton.TabIndex = 2;
@@ -70,7 +71,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::GuessMelodyWindowsFormsApp.Properties.Resources.melodiBild;
-            this.ClientSize = new System.Drawing.Size(407, 363);
+            this.ClientSize = new System.Drawing.Size(489, 363);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.parametrsButton);
             this.Controls.Add(this.playButton);
