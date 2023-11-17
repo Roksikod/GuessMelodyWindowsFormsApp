4efc9b136f35e4d5d43db2ad0cd60176d055ccb8
Roksana Zalesskaya <Ksroksana@bk.ru>
2023-11-17 20:53:35 +0300

    Added buttons on the Main Form, created Close()

--- GuessMelodyWindowsFormsApp/MainForm.Designer.cs
+++ GuessMelodyWindowsFormsApp/MainForm.Designer.cs
@@ -28,13 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.playButton = new System.Windows.Forms.Button();
+            this.parametrsButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.playButton.Location = new System.Drawing.Point(116, 55);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(168, 64);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Spielen";
+            this.playButton.UseVisualStyleBackColor = true;
+            // 
+            // parametrsButton
+            // 
+            this.parametrsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.parametrsButton.Location = new System.Drawing.Point(116, 138);
+            this.parametrsButton.Name = "parametrsButton";
+            this.parametrsButton.Size = new System.Drawing.Size(168, 64);
+            this.parametrsButton.TabIndex = 1;
+            this.parametrsButton.Text = "Einstellungen";
+            this.parametrsButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exitButton.Location = new System.Drawing.Point(116, 230);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(168, 64);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Verlassen";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::GuessMelodyWindowsFormsApp.Properties.Resources.melodiBild;
+            this.ClientSize = new System.Drawing.Size(407, 363);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.parametrsButton);
+            this.Controls.Add(this.playButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "Erraten Sie die Melodie!";
             this.ResumeLayout(false);
@@ -42,6 +82,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button parametrsButton;
+        private System.Windows.Forms.Button exitButton;
     }
 }
 
