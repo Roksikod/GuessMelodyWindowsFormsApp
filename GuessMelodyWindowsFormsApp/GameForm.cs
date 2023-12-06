d8764e045422854e1a58a6f4fa85a409f951daa6
Roksana Zalesskaya <Ksroksana@bk.ru>
2023-12-07 00:20:21 +0300

    Timer and progressbar

--- GuessMelodyWindowsFormsApp/GameForm.cs
+++ GuessMelodyWindowsFormsApp/GameForm.cs
@@ -18,34 +18,49 @@ namespace GuessMelodyWindowsFormsApp
             winMediaPlayer.URL = Game.gameList[numberSong];
             //winMediaPlayer.Ctlcontrols.play();
             Game.gameList.RemoveAt(numberSong);
-
-
+            countMelodyLabel.Text = Game.gameList.Count.ToString();
         }
 
         private void nextButton_Click(object sender, EventArgs e)
         {
+            timer.Start();
             MakeMusic();
         }
 
         private void GameForm_Load(object sender, EventArgs e)
         {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
+            countMelodyLabel.Text = Game.gameList.Count.ToString();
+            progressBar.Value = 0;
+            progressBar.Minimum = 0;
+            progressBar.Maximum = Game.gameDuration;
         }
 
         private void GameForm_FormClosed(object sender, FormClosedEventArgs e)
         {
+            timer.Stop();
             winMediaPlayer.Ctlcontrols.stop();
+        }
+       
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            progressBar.Value++;
 
+            if (progressBar.Value == progressBar.Maximum)
+            {
+                timer.Stop();
+            }
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            winMediaPlayer.Ctlcontrols.pause();
+        }
+
+        private void continueButton_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            winMediaPlayer.Ctlcontrols.play();
         }
     }
 }
